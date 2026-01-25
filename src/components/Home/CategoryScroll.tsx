92ef2e8f9b3d45d19f4f95874c84e132f663fad9
midambo <gregmidambo2@gmail.com>
2026-01-25 03:07:12 +0300

    fix homepage error

--- src/components/Home/CategoryScroll.tsx
+++ src/components/Home/CategoryScroll.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 import { getProductImageUrl } from "@/utils/product-images";
 
 interface Category {
